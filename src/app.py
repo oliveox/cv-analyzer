9dada15fa030022366ff2ce8e3307764086c649c
Razvan Gorgan <razvan.gorgan@sectorlabs.ro>
2023-01-17 20:59:10 +0200

    Hardcode CV and API return JSON format while caring for custom exceptions

--- src/app.py
+++ src/app.py
@@ -1,19 +1,67 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+CV = {
+    "personal": [
+        {"year": "1990", "name": "John Doe"},
+    ],
+    "experience": [
+        {"year": "2020 - 2023", "company": "Google"},
+        {"year": "2023 - Present", "company": "Cegeka"},
+    ],
+    "education": [
+        {"year": "2010 - 2014", "school": "High-School"},
+        {"year": "2014 - 2018", "school": "MIT"},
+    ],
+}
+
 
 @app.route("/")
 def index():
     return "Let's analyze your CV!"
 
+
 @app.get('/personal')
 def get_personal_section():
-    return 'personal section'
+    try:
+        return jsonify(CV['personal'])
+    except KeyError:
+        return jsonify({
+            "error": "The CV doesn't contain an [personal] section"
+        })
+    except Exception as e:
+        print(f"Unexpected error occured: {e}")
+        return jsonify({
+            "error": "Unexpected error"
+        })
+
 
 @app.get('/experience')
 def get_experience_section():
-    return 'experience section'
+    try:
+        return jsonify(CV['experience'])
+    except KeyError:
+        return jsonify({
+            "error": "The CV doesn't contain an [experience] section"
+        })
+    except Exception as e:
+        print(f"Unexpected error occured: {e}")
+        return jsonify({
+            "error": "Unexpected error"
+        })
+
 
 @app.get('/education')
 def get_education_section():
-    return 'education section'
+    try:
+        return jsonify(CV['educatio'])
+    except KeyError:
+        return jsonify({
+            "error": "The CV doesn't contain an [education] section"
+        })
+    except Exception as e:
+        print(f"Unexpected error occured: {e}")
+        return jsonify({
+            "error": "Unexpected error"
+        })
